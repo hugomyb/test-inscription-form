5e3629efd435525ae80b312c09687e6f5fc1c6bc
Hugo Mayonobe <hugomayonobe@gmail.com>
2024-10-08 16:22:14 +0200

    add coverage on tests

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,19 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { importProvidersFrom } from '@angular/core';
-import { ToastrModule } from 'ngx-toastr';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
+import {provideRouter} from '@angular/router';
+import {importProvidersFrom} from '@angular/core';
+import {ToastrModule} from 'ngx-toastr';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import { routes } from './app.routes';
+import {routes} from './app.routes';
+import {HashLocationStrategy, LocationStrategy} from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
+    provideZoneChangeDetection({eventCoalescing: true}), provideRouter(routes),
     importProvidersFrom(
       BrowserAnimationsModule,
       ToastrModule.forRoot()
-    )
+    ),
+    {provide: LocationStrategy, useClass: HashLocationStrategy}
   ]
 };
